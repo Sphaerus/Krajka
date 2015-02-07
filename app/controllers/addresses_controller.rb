211a1725090b2c3ea018ab63dab5c3d381d2a985
Sphaerus <wyfsm1@gmail.com>
2015-02-07 18:16:32 +0100

    crud adresów

--- app/controllers/addresses_controller.rb
+++ app/controllers/addresses_controller.rb
@@ -1,9 +1,52 @@
 class AddressesController < ApplicationController
+  before_action :set_address, only: [:edit, :update, :destroy]
+  
   def index
     @addresses = Address.all
   end
   
   def new
     @address = current_user.addresses.build
+  end
+  
+  def edit
+    @address = Address.find(params[:id])
+  end
+  
+  def create
+    @address = Address.new(address_params)
+    
+    respond_to do |format|
+      if @address.save
+        format.html { redirect_to addresses_path}
+      else
+        format.html {render action: "new"}
+      end
+    end
+  end
+  
+  def update
+    respond_to do |format|
+      if @address.update_attributes(address_params)
+        format.html { redirect_to addresses_path, notice: "Adres pomyślnie zatkualizowano"}
+      else
+        format.html { render action: "edit"}
+      end
+    end
+  end
+  
+  def destroy
+    @address.destroy
+    redirect_to addresses_path, notice: "Adres usunięty"
+  end
+  
+  private
+  
+  def set_address
+    @address = Address.find(params[:id])
+  end
+  
+  def address_params
+    params.require(:address).permit(:name, :street, :other, :town, :country, :province, :postal_code)
   end
 end
